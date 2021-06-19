29b8eb967df35026399f86fc600ba12fe3d1d4ab
bky373 <bo373@naver.com>
2021-06-19 17:27:21 +0900

    Feat : 데코레이터 패턴 구현

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -2,7 +2,7 @@ package com.company.design;
 
 import com.company.design.adapter.*;
 import com.company.design.aop.AopBrowser;
-import com.company.design.proxy.Browser;
+import com.company.design.decorator.*;
 import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
@@ -12,6 +12,10 @@ import com.company.design.singleton.SocketClient;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
+    // 어댑터 패턴 연습을 위한 콘센트 연결 메소드
+    public static void connect(Electronic110V electronic110V) {
+        electronic110V.powerOn();
+    }
 
     public static void main(String[] args) {
 
@@ -79,10 +83,21 @@ public class Main {
         aopBrower.show();
         System.out.println("loading time : " + end.get());
 
-    }
 
-    // 콘센트
-    public static void connect(Electronic110V electronic110V) {
-        electronic110V.powerOn();
+        // 데코레이터 패턴
+        ICar audi = new Audi(1000);
+        audi.showPrice();
+
+        // a3
+        ICar audi3 = new A3(audi, "A3");
+        audi3.showPrice();
+
+        // a4
+        ICar audi4 = new A4(audi, "A4");
+        audi4.showPrice();
+
+        // a5
+        ICar audi5 = new A5(audi, "A5");
+        audi5.showPrice();
     }
 }
